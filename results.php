1d0ccfeade3ef705cc0f66adc8b83a19e80af68f
Matthew <flarebrass166@gmail.com>
2016-11-03 12:23:11 -0600

    update

--- results.php
+++ results.php
@@ -22,20 +22,48 @@ $request = $_POST['Request'];
 
 if($request == "Create"){
   $insert = "INSERT INTO Library . FriendsAndFamily (FirstName, LastName, PhoneNumber, Address, City, State, 
-              Zip, Birthdate, UserName, Password, Sex, Relationship) VALUES ('" . mysql_fix_string($con, $_POST['FirstName']) . "',
-              '" . mysql_fix_string($con, $_POST['LastName']) . "',  '" .  mysql_fix_string($con, $_POST['PhoneNumber']) . "',
-              '" . mysql_fix_string($con, $_POST['Address']) . "',  '" .  mysql_fix_string($con, $_POST['City']) .  "',
-              '" . mysql_fix_string($con, $_POST['State']) . "', '" .  mysql_fix_string($con, $_POST['Zip']) . "',
-              '" . mysql_fix_string($con, $_POST['Birthdate']) . "', '" . mysql_fix_string($con, $_POST['UserName']) . "',
-              '" . hash("ripemd128", $_POST['Password']) . "', '" . mysql_fix_string($con, $_POST['Sex']) . "',
-              '" . mysql_fix_string($con, $_POST['Relationship']) . "',
-               NOW(), 0";
+              Zip, Birthdate, UserName, Password, Sex, Relationship) VALUES ('" . mysql_fix_string($con, $firstName) . "',
+              '" . mysql_fix_string($con, $lastName) . "',  '" .  mysql_fix_string($con, $phone) . "',
+              '" . mysql_fix_string($con, $address) . "',  '" .  mysql_fix_string($con, $city) .  "',
+              '" . mysql_fix_string($con, $state) . "', '" .  mysql_fix_string($con, $zip) . "',
+              '" . mysql_fix_string($con, $birthdate) . "', '" . mysql_fix_string($con, $userName) . "',
+              '" . hash("ripemd128", $_POST['Password']) . "', '" . mysql_fix_string($con, $sex) . "',
+              '" . mysql_fix_string($con, $relationship) . "')";
 
     $result = $con->query($insert);
 
         if (!$result){
-            echo "Something went wrong while vreating new record, please try again";
+            echo "Something went wrong while creating new record, please try again";
+            echo $insert;
         }else{
             echo "Record successfully registered.";
         }
+}
+else if ($request == "Search"){
+    $select = "SELECT FirstName, LastName, PhoneNumber, Address, City, State, 
+              Zip, Birthdate, UserName, Password, Sex, Relationship FROM Library . FriendsAndFamily WHERE  FirstName = '$firstName' and LastName = '$lastName'";
+
+    $return = $con->query($select);
+
+    if(!$return){
+        echo "Something went wrong while searching, please try again";
+        echo $select;
+    }
+
+    echo "<table><th>First Name</th><th>Last Name</th><th>City</th><th>State</th><th>Zip</th><th>Phone Number</th><th>Sex</th><th>Birth date</th><th>Relationship</th>";
+    while ($row = $return->fetch_assoc()) {
+        echo "<tr><td> First Name: " . $row['FirstName']
+            . "</td><td> Last Name: " . $row['LastName']
+            . "</td><td> City: " . $row['City']
+            . "<tr><td> Zip: " . $row['Zip']
+            . "</td><td> Sex: " . $row['Sex']
+            . "</td><td> Birth date: " . $row['Birthdate']
+            . "</td><td> Relationship: " . $row['Relationship'] . "</td></tr>";
+    }
+    echo "</table>";
+
+    if($request == "Update"){
+        $update = "UPDATE Library . FriendsAndFamily ";
+    }
+    mysqli_close($con);
 }
